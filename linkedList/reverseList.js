89b8c26f81377ec820dbc7d0ed08c20e77865579
Karanbairwaa12 <karanbairwaa@gmail.com>
2026-02-06 18:12:54 +0530

    reverse list

---
+++ linkedList/reverseList.js
@@ -0,0 +1,10 @@
+const { buildList } = require("./buildList")
+const { printList } = require("./printList")
+
+var reverse = function(head1) {
+    let curr = head1;
+    
+    return curr;
+}
+let head1 = buildList([1,2,3,4,5,6])
+printList(reverse(head1))
